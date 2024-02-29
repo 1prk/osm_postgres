f702324f9f512c065e01a9ed1550cff3652a5058
Iwan Porojkow <iwan.porojkow@tu-dresden.de>
2024-02-29 16:47:32 +0100

    Update relations.sql

--- relations.sql
+++ relations.sql
@@ -1,20 +1,25 @@
---EXPERIMENTAL
---dauert noch sehr lange, um Relationen und ihre Geometrien zu extrahieren
 SELECT 
+
     w.osm_id,
 	w.highway,
     st_setsrid(w.way, 3857) as geom,
     n.id as relation_id,
     rel_ways,
-    n.tags
+    n.tags::hstore
+
 FROM 
+
     planet_osm_rels n
 CROSS JOIN LATERAL
     unnest(n.parts) AS rel_ways
+
 LEFT JOIN
     planet_osm_line w ON rel_ways = w.osm_id
 JOIN 
 	vg250_gen_transformed g ON 
 	st_contains(g.transformed_geom, st_setsrid(w.way, 3857))
+
 WHERE 
-	g."GEN" = 'Oschatz';
+	g."GEN" = 'Dresden'
+
+	AND 'bicycle'=ANY(n.tags)
